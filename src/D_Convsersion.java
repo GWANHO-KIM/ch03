ba6bc7b4668716795338c7ddc7409347abc77b80
GWANHOKIM <xo99dhks@gmail.com>
2019-07-08 09:26:20 +0900

    copy

---
+++ src/D_Convsersion.java
@@ -0,0 +1,21 @@
+// 명시적 형 변환의 예시
+
+public class D_Convsersion 
+{
+	public static void main(String[] args)
+	{
+		double pi = 3.1415;
+		int wholenumber = (int)pi;
+		System.out.println(wholenumber);
+		
+		long num1 = 3000000007L;
+		int num2 = (int)num1;
+		System.out.println(num2);
+		
+		short num3 = 1;
+		short num4 = 2;
+		short num5 = (short)(num3 + num4);
+		System.out.println(num5);
+	}
+
+}
